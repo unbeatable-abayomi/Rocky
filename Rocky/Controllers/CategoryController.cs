53538a8b693a1d6fdf893bbb4424a419812b3136
abayomi igwubor <abayomi.igwubor@libmot.com>
2021-07-26 16:27:13 +0100

    first commit

---
+++ Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category.ToList();
+
+
+            return View(objList);
+        }
+        [HttpGet] //GET CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost] //POST CREATE
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Category.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Rocky/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        //[DisplayName("Display Order Now")]
+        //What ever you put in display name is what show on the view, as long as you have used the asp net label tag on the view page
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]  //What ever you put in display name is what show on the view, as long as you have used the asp net label tag on the view page
+        public int DisplayOrder { get; set; }
+    }
+}
