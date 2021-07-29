bd11b7ce34083e7db86c0d856307faee3ecbe899
abayomi igwubor <abayomi.igwubor@libmot.com>
2021-07-29 17:21:36 +0100

    addedValidationForApplicationType

--- Rocky/Models/ApplicationType.cs
+++ Rocky/Models/ApplicationType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,7 +11,8 @@ namespace Rocky.Models
     {
         [Key]
         public int Id { get; set; }
-
+        [Required]
+        [DisplayName("Application Type")]
         public string Name { get; set; }
     }
 }
