335452bb1dc561a31c664ebfc83d82c49d2e7a4b
abayomi igwubor <abayomi.igwubor@libmot.com>
2021-07-30 16:52:59 +0100

    addedClientSideValidation

--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -41,5 +41,33 @@ namespace Rocky.Controllers
             }
             return View(obj);
         }
+
+        [HttpGet] //GET EDIT
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 00)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost] //POST EDIT
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {//the update method will only work if there is an ID associated , so that why we but it as a hidden field in the view
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
